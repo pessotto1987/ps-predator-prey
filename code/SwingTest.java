cd067d3a5d0ac0ceb80e40b9ee01c3c57cdf61c1
tsiguel@gmail.com <tsiguel@gmail.com@02657379-8067-272f-a810-6bace14edcbe>
2011-10-20 11:26:22 +0000

    First shot at the gui. so far it just takes the values and is re sizable. Comments and updates to follow/

---
+++ code/SwingTest.java
@@ -0,0 +1,126 @@
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.*;
+
+class TestFrame extends JFrame
+{
+	JTextField hareBirthRate, pumaBirthRate, pumaPredationRate, 
+	pumaDiffusionRate, pumaDeathRate, hareDiffusionRate, timeStep;
+	
+	double parameters[] = new double[7];
+
+	public void read()
+	{
+		parameters[0] = Double.parseDouble(hareBirthRate.getText());
+		parameters[1] = Double.parseDouble(pumaBirthRate.getText());
+		parameters[2] = Double.parseDouble(pumaPredationRate.getText());
+		parameters[3] = Double.parseDouble(pumaDiffusionRate.getText());
+		parameters[4] = Double.parseDouble(pumaDeathRate.getText());
+		parameters[5] = Double.parseDouble(hareDiffusionRate.getText());
+		parameters[6] = Double.parseDouble(timeStep.getText());
+	}
+	TestFrame()
+	{
+		double inputs[] = new double[7];
+				Container content = this.getContentPane();
+		//content.setBackground(Color.lightGray);
+		/*content.setLayout(new BoxLayout(content,BoxLayout.X_AXIS)); 
+		JPanel pan1 = new JPanel();
+		pan1.setLayout(new BoxLayout(pan1,BoxLayout.Y_AXIS));
+		
+		JPanel pan2 = new JPanel();
+		pan2.setLayout(new BoxLayout(pan2,BoxLayout.Y_AXIS));
+		
+		JPanel pan3 = new JPanel();
+		pan3.setLayout(new BoxLayout(pan3,BoxLayout.Y_AXIS));
+		*/
+		content.setLayout(new GridLayout(7,4));
+		ButtonGroup choise = new ButtonGroup();
+		JRadioButton preSet = new JRadioButton("PreSet");
+		JRadioButton range = new JRadioButton("Range");
+		JButton start = new JButton("Start");
+		choise.add(preSet);
+		choise.add(range);
+		
+		this.hareBirthRate = new JTextField("3");
+		//hareBirthRate.setPreferredSize(new Dimension(50, 25));
+		this.pumaBirthRate = new JTextField("7");
+		this.pumaPredationRate = new JTextField("7");
+		this.pumaDiffusionRate = new JTextField("7");
+		this.pumaDeathRate = new JTextField("7");
+		this.hareDiffusionRate = new JTextField("3");
+		this.timeStep = new JTextField("3");
+		
+		JLabel hareBirthRateL = new JLabel("Hare Birth Rate");
+		JLabel pumaBirthRateL = new JLabel("Puma Birth Rate");
+		JLabel pumaPredationRateL = new JLabel("Puma Predation Rate");
+		JLabel pumaDiffusionRateL = new JLabel("Puma Diffusion Rate");
+		JLabel pumaDeathRateL = new JLabel("Puma Death Rate");
+		JLabel hareDiffusionRateL = new JLabel("Hare Diffusion Rate");
+		JLabel timeStepL = new JLabel("Time Step");
+		
+		content.add(hareBirthRateL);
+		content.add(hareBirthRate);
+		//content.add(new JLabel(""));
+		content.add(start);
+		start.addActionListener(new ActionListener()
+			{
+ 
+				public void actionPerformed(ActionEvent e)
+				{
+					read();
+				}
+			});
+		
+		content.add(pumaBirthRateL);		
+		content.add(pumaBirthRate);
+		content.add(new JLabel(""));
+
+		content.add(pumaPredationRateL);
+		content.add(pumaPredationRate);
+		content.add(new JLabel(""));
+		
+		content.add(pumaDiffusionRateL);
+		content.add(pumaDiffusionRate);
+		content.add(new JLabel(""));
+		
+		content.add(pumaDeathRateL);
+		content.add(pumaDeathRate);
+		content.add(new JLabel(""));
+		
+		content.add(hareDiffusionRateL);
+		content.add(hareDiffusionRate);
+		content.add(range);
+		
+		content.add(timeStepL);
+		content.add(timeStep);
+		content.add(preSet);
+		
+
+
+
+
+		
+		
+		
+		
+
+		
+		
+		//JLabel jlbHelloWorld = new JLabel("Hello World");
+		//add(jlbHelloWorld);
+		this.setSize(400, 200);
+		setVisible(true);
+	}
+}
+public class SwingTest
+{
+	public static void main(String args[])
+	{
+		new TestFrame();
+		System.out.println("test");
+	}
+}

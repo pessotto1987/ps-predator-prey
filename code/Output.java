5a473305c00ef093cc41d09ca16cb85a0426a93d
milena.pawlik@hotmail.com <milena.pawlik@hotmail.com@02657379-8067-272f-a810-6bace14edcbe>
2011-10-24 17:34:19 +0000

    Output.java updated

--- code/Output.java
+++ code/Output.java
@@ -2,7 +2,7 @@ import java.io.*;
 /**
  * Method to write output in ppm files.
  * @author Milena Put your name here if you work on this class
- * @version 2.0, October, 24th 2011
+ * @version 3.0, October, 24th 2011
  * @since 1.0
  */
 
@@ -16,21 +16,13 @@ public class Output{
 
     int [][][] cell;
     int [][] density;   
-
-   
     
     /**
-     *Constructors for objects
+     *Constructor for density
      * @param d density
-     * @param x Row index
-     * @param y Column index
-     * @param red pixel
-     * @param green pixel
-     * @param blue pixel
+    
      */
-
    
-      
     public void setDensity(int d)
     {
 	density[x][y] = d;
@@ -39,6 +31,19 @@ public class Output{
     {
 	return density;
     }
+
+
+
+    /**
+     * Constructor for the image cells. The third dimension [3] is introduced
+     * to account for the contribution of red/green/blue 
+     * colours to each image cell.
+     * @param x Row index
+     * @param y Column index
+     * @param red pixel
+     * @param green pixel
+     * @param blue pixel
+     **/
 
     public void setImageCell(int x, int y, int red, int green, int blue)
     { 	
@@ -53,17 +58,18 @@ public class Output{
 
 
     /**
-     * Method to write densities into a 'red' PPM file. 
+     * Method to write densities into a PPM file
      * In the final output, variations in density with position will be seen as 
-     * different shades of red (grid cells with zero density will appear white).
-     * Water cells set to black (need the density in water cells to be negative).
+     * different shades of red/green/blue 
+     * (grid cells with zero density will appear white)
+     * Water cells set to black (need the density in water cells to be negative)
      * @param outputName 
      * @param density 
      * 
      **/
 
     
-    public void printPpmRed(String outputName, int[][] density) throws Exception { 	
+    public void printPpm(String outputName, int[][] density, String colour) throws Exception { 	
 
 	for(x=0; x<density.length; x++)
 	    {
@@ -96,9 +102,24 @@ public class Output{
 		    
 			else
 			    {
-				cell[x][y][0] = density[x][y];
-				cell[x][y][1] = 0;
-				cell[x][y][2] = 0;
+				if(colour=="red")
+				    {
+					cell[x][y][0] = density[x][y];
+					cell[x][y][1] = 0;
+					cell[x][y][2] = 0;
+				    }
+				if(colour=="green")
+				    {
+					cell[x][y][0] = 0;
+					cell[x][y][1] = density[x][y];
+					cell[x][y][2] = 0;
+				    }
+				if(colour=="blue")
+				    {
+					cell[x][y][0] = 0;
+					cell[x][y][1] = 0;
+					cell[x][y][2] = density[x][y];
+				    }
 			    }
 
 		    }
@@ -127,79 +148,6 @@ public class Output{
 	// TODO - sort out the spaces
 
 
-
-
- /**
-     * Method to write densities into a 'green' PPM file. 
-     * Essentially same as for 'red' but with different pixel colours being picked up.
-     * In the final output, variations in density with position will be seen as 
-     * different shades of green (again grid cells with zero density will appear white).
-     * Water cells set to black (need the density in water cells to be negative).
-     * @param outputName 
-     * @param density 
-     * 
-     **/
-
-
- public void printPpmGreen(String outputName, int[][] density) throws Exception { 	
-
-	for(x=0; x<density.length; x++)
-	    {
-		for(y=0; y<density[0].length; y++)
-		    {
-			if(density[x][y]>maxValue)
-			    {
-				maxValue = density[x][y];
-			    }
-		    }
-	    }
-
-	
-        cell = new int[density.length][density[0].length][3];
-	
-
-	for(x=0; x<density.length; x++)
-	    {
-		for(y=0; y<density[0].length; y++)
-		    {
-
-			
-			if(density[x][y]<=-1)
-			    {
-				cell[x][y][0] = maxValue;
-				cell[x][y][1] = maxValue;
-				cell[x][y][2] = maxValue;
-			    }
-		    
-			else
-			    {
-				cell[x][y][0] = 0;
-				cell[x][y][1] = density[x][y];
-				cell[x][y][2] = 0;
-			    }
-
-		    }
-	    }		
-
-	PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(outputName)));
-
-			out.printf("P3");
-			out.printf("\n");
-		       	out.printf(""+density.length+" "+density[0].length);
-		       	out.printf("\n");
-		       	out.printf(""+maxValue);
-		       	out.printf("\n");
-
-	for(x=0; x<density.length; x++)
-	    {
-		for(y=0; y<density[0].length; y++)
-		    {				
-			out.printf("%s", cell[x][y][0]+" "+cell[x][y][1]+" "+cell[x][y][2]+"  ");	       		    }
-		out.printf("\n");
-	    }
-	out.close();    	
-    }
-    
 
 }	    
 	

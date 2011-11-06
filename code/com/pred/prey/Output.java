4e41eb33c0102f0111eacfba70f94544fd97b73c
asianuxx <asianuxx@02657379-8067-272f-a810-6bace14edcbe>
2011-11-06 10:19:38 +0000

    New test case for Output.java. Slight modification to output.java, only in the way it deals with exceptions.

--- code/com/pred/prey/Output.java
+++ code/com/pred/prey/Output.java
@@ -1,143 +1,149 @@
 package com.pred.prey;
+
 import java.io.*;
+
 /**
  * Method to write output in ppm files.
+ * 
  * @author Milena Put your name here if you work on this class
  * @version 6.0, October, 28th 2011
  * @since 1.0
  */
 
-public class Output{
+public class Output {
+	int x, y;
+	double maxValue, minValue;
 
- 
-    int x, y;
-    double maxValue, minValue;
-   
-    int [][][] cell;
-    double [][] density;   
-        
-    public Output()	{
-    	
-    }
+	int[][][] cell;
+	double[][] density;
 
-
-    /**
-     * Constructor for the image cells. The third dimension [3] is introduced
-     * to account for the contribution of red/green/blue 
-     * colours to each image cell.
-     * @param x Row index
-     * @param y Column index
-     * @param red pixel
-     * @param green pixel
-     * @param blue pixel
-     **/
-
-    /**
-     * Method to write densities into a PPM file
-     * In the final output, water will be seen as blue and land as green cells.
-     * The change in density of animals with position will be represented by different shades of grey
-     * (the higher the density the darker the colour).
-     * @param outputName 
-     * @param density 
-     * @param colour optional
-     **/
-
-    
-    public void printPpm(String outputName, double[][] density, int[][] neighbours) throws Exception { 	
-
+	public Output() {
+	}
 
 	/**
-	 * Calculate the maximum density.
+	 * Constructor for the image cells. The third dimension [3] is introduced to
+	 * account for the contribution of red/green/blue colours to each image
+	 * cell.
+	 * 
+	 * @param x
+	 *            Row index
+	 * @param y
+	 *            Column index
+	 * @param red
+	 *            pixel
+	 * @param green
+	 *            pixel
+	 * @param blue
+	 *            pixel
 	 **/
-	maxValue=0;
-	minValue=10; // have to guess at a good starting point here
-	for(x=1; x<(density.length-1); x++)
-	    {
-		for(y=1; y<(density[0].length-1); y++)
-		    {
-			if(density[y][x]>maxValue)
-			    {
-				maxValue = density[y][x];
-			    }
-			if((density[y][x]<minValue) && (neighbours[y][x]!=-1))
-			    {
-				minValue = density[y][x];
-			    }
-		    }
-	    }
-	double scale=250/(maxValue-minValue); //actual max will be 255, leave room for error
-		
-	 /**
-	 * Print the values in a ppm file.
-	 **/
-	 			
-	BufferedWriter bufferedWriter = null;
-   bufferedWriter= new BufferedWriter(new FileWriter(outputName,true));
-	
-	bufferedWriter.write("P3");
-		bufferedWriter.newLine();
-	bufferedWriter.write((density.length-2)+" "+(density[0].length-2));
-		bufferedWriter.newLine();
-	bufferedWriter.write("255");
-		bufferedWriter.newLine();
-	
-	for(y=1; y<(density[0].length-1); y++)
-	{
-		for(x=1; x<(density.length-1); x++)
-		{				
-			/**
-			 * Fill water cells with blue.
-			 **/
-				if(neighbours[y][x]==-1)
-				{
-				bufferedWriter.write("0 0 255 ");
-				}
-				else{
-				
-				bufferedWriter.write((int)((density[y][x]-minValue)*scale)+" "
-				+(int)((density[y][x]-minValue)*scale)+" "+(int)((density[y][x]-minValue)*scale)+" ");
 
+	/**
+	 * Method to write densities into a PPM file In the final output, water will
+	 * be seen as blue and land as green cells. The change in density of animals
+	 * with position will be represented by different shades of grey (the higher
+	 * the density the darker the colour).
+	 * 
+	 * @param outputName
+	 * @param density
+	 * @param colour
+	 *            optional
+	 **/
+	public void printPpm(String outputName, double[][] density,
+			int[][] neighbours) {
+		/**
+		 * Calculate the maximum density.
+		 **/
+		maxValue = 0;
+		minValue = 10; // have to guess at a good starting point here
+		for (x = 1; x < (density.length - 1); x++) {
+			for (y = 1; y < (density[0].length - 1); y++) {
+				if (density[y][x] > maxValue) {
+					maxValue = density[y][x];
 				}
-					    
-				
+				if ((density[y][x] < minValue) && (neighbours[y][x] != -1)) {
+					minValue = density[y][x];
+				}
+			}
 		}
-		bufferedWriter.newLine();
+		double scale = 250 / (maxValue - minValue); // actual max will be 255,
+													// leave room for error
+
+		/**
+		 * Print the values in a ppm file.
+		 **/
+
+		BufferedWriter bufferedWriter = null;
+		try {
+			bufferedWriter = new BufferedWriter(
+					new FileWriter(outputName, true));
+			bufferedWriter.write("P3");
+
+			bufferedWriter.newLine();
+			bufferedWriter.write((density.length - 2) + " "
+					+ (density[0].length - 2));
+			bufferedWriter.newLine();
+			bufferedWriter.write("255");
+			bufferedWriter.newLine();
+
+			for (y = 1; y < (density[0].length - 1); y++) {
+				for (x = 1; x < (density.length - 1); x++) {
+					/**
+					 * Fill water cells with blue.
+					 **/
+					if (neighbours[y][x] == -1) {
+						bufferedWriter.write("0 0 255 ");
+					} else {
+						bufferedWriter
+								.write((int) ((density[y][x] - minValue) * scale)
+										+ " "
+										+ (int) ((density[y][x] - minValue) * scale)
+										+ " "
+										+ (int) ((density[y][x] - minValue) * scale)
+										+ " ");
+					}
+
+				}
+				bufferedWriter.newLine();
+			}
+			bufferedWriter.flush();
+			bufferedWriter.close();
+		} catch (IOException ioe) {
+			ioe.getMessage();
+		}
 	}
-	bufferedWriter.flush();
-	bufferedWriter.close();    	
-   }
-    
 
-
-	
-    /**
-     * Method to write average density with corresponding time to a file
-     * @param outputName
-     * @param density
-     * @param time
-     **/
-
-    public void printMeanDensity(String outputName, double[][] density, double time) throws Exception
-    {
-    	
-        double sum = 0;
-        double average;
-	
-       	PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(outputName,true)));
-	
-       	for (x=1; x<(density.length-1); x++)
-       	{
-       		for (y=1; y<(density[x].length-1); y++)
-       		{       			
-       			sum += density[y][x];
-		    }
-	    }
+	/**
+	 * Method to write average density with corresponding time to a file
+	 * 
+	 * @param outputName
+	 * @param density
+	 * @param time
+	 **/
+	public void printMeanDensity(String outputName, double[][] density,
+			double time) {		
+		double sum = 0;
+		double average;
+		PrintWriter out = null;
 		
-	 average = sum/((double)(density.length-1)*(double)(density[0].length-1));
-	 
-	 out.printf(time+" "+average);
-	 out.printf("\n");
-	 out.close();
-    }
-  
-}	    
+		try {
+			out = new PrintWriter(new BufferedWriter(new FileWriter(outputName,
+					true)));
+		} catch (IOException ioe) {
+			ioe.getMessage();
+		}
+
+		for (x = 1; x < (density.length - 1); x++) {
+			for (y = 1; y < (density[x].length - 1); y++) {
+				sum += density[y][x];
+			}
+		}
+
+		average = sum
+				/ ((double) (density.length - 1) * (double) (density[0].length - 1));
+
+		out.printf(time + " " + average);
+		out.printf("\n");
+		out.close();
+	}
+
+}
---
+++ code/com/pred/tests/TestOutput.java
@@ -0,0 +1,124 @@
+package com.pred.tests;
+
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.pred.prey.Output;
+
+/**
+ * @author Jorge M.
+ * 
+ */
+public class TestOutput {
+	private Output output;
+	private double[][] density = { { 0.3, 0.4 }, { 0.2, 0.5 } };
+	private int[][] neighbours = { { 3, 4 }, { 2, 4 } };
+	private int time;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		time = 2;
+		output = new Output();
+		output.printPpm("testOutputFile.ppm", density, neighbours);
+		output.printMeanDensity("testOutputFile2.ppm", density, time);
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		output = null;
+		density = null;
+		neighbours = null;
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.pred.prey.Output#printPpm(java.lang.String, double[][], int[][])}
+	 * .
+	 */
+	@Test
+	public final void testPrintPpm() {
+		FileInputStream fstream;
+		String strLine;
+		
+		try {
+			fstream = new FileInputStream("./test/testOutputFile.ppm");
+			assertNotNull(fstream);
+			DataInputStream in = new DataInputStream(fstream);
+			assertNotNull(in);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			assertNotNull(br);
+
+			try {
+				while ((strLine = br.readLine()) != null) {
+					assertNotNull(strLine);
+				}
+			} catch (IOException ioe) {
+				ioe.getMessage();
+			}
+
+			try {
+				fstream.close();
+				in.close();
+				br.close();
+			} catch (IOException ioe) {
+				ioe.getMessage();
+			}
+		} catch (FileNotFoundException fnfe) {
+			fnfe.getMessage();
+		}
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.pred.prey.Output#printMeanDensity(java.lang.String, double[][], double)}
+	 * .
+	 */
+	@Test
+	public final void testPrintMeanDensity() {
+		FileInputStream fstream;
+		String strLine;
+		
+		try {
+			fstream = new FileInputStream("./test/testOutputFile2.ppm");
+			assertNotNull(fstream);
+			DataInputStream in = new DataInputStream(fstream);
+			assertNotNull(in);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			assertNotNull(br);
+	
+			try {
+				while ((strLine = br.readLine()) != null) {
+					assertNotNull(strLine);
+				}
+			} catch (IOException ioe) {
+				ioe.getMessage();
+			}
+
+			try {
+				fstream.close();
+				in.close();
+				br.close();
+			} catch (IOException ioe) {
+				ioe.getMessage();
+			}
+		} catch (FileNotFoundException fnfe) {
+			fnfe.getMessage();
+		}
+	}
+}

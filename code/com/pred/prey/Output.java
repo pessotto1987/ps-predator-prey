a9c84046d3076f5b9da3ae1d4682f873e587652d
milena.pawlik@hotmail.com <milena.pawlik@hotmail.com@02657379-8067-272f-a810-6bace14edcbe>
2011-11-08 23:56:42 +0000

    I've added some comments and tried to tidy up the output class a bit

--- code/com/pred/prey/Output.java
+++ code/com/pred/prey/Output.java
@@ -3,14 +3,15 @@ package com.pred.prey;
 import java.io.*;
 
 /**
- * Method to write output in ppm files.
+ * This class contains methods to create/clean output directories and to write data to output files. 
  * 
- * @author Milena, Tom Put your name here if you work on this class
+ * @author Milena, Tom 
  * @version 6.0, October, 28th 2011
  * @since 1.0
- */
+ **/
 
 public class Output {
+	
 	int x, y;
 	double maxValue, minValue;
 	boolean direrror = false;
@@ -20,34 +21,44 @@ public class Output {
 	}
 
   /**
-  * Creates directory 'dirname' if it doesn't exists,
-  * cleans it if it does.
+  * Method to create a directory 'dirname' if it doesn't exists already,
+  * or to clean it if it does.
   * @param dirname
-  */
+  **/
+	
 	public void cleanDirectory(String dirname) {
 	
 		File directory = new File(dirname);
 
-		if(directory.exists()) {
-
+		/**
+		 *Check if directory 'dirname' exists and if it does, delete all files it contains.
+		 **/
+		if(directory.exists()) 
+		{
+			File[] files = directory.listFiles();
 		
-		File[] files = directory.listFiles();
+			System.out.println("Cleaning "+dirname+" directory...");
 		
-		System.out.println("Cleaning "+dirname+" directory...");
-		
-		for (File file : files) {
-			if (!file.delete()) {
-			//failed to delete file
-			System.out.println("Failed to delete "+file);
-			}
-		}	
+			for (File file : files) 
+			{
+				if (!file.delete())
+				{
+					//failed to delete file
+					System.out.println("Failed to delete "+file);
+				}
+			}	
 
 		}
+		/**
+		 *Otherwise create directory 'dirname'.
+		 **/
 		else 
 		{
-		System.out.println("No output directory found, creating "+dirname+" directory.");
-			if(!directory.mkdir()){
-			direrror=true;
+			System.out.println("No output directory found, creating "+dirname+" directory.");
+			
+			if(!directory.mkdir())
+			{
+				direrror=true;
 			}
 		}
 	
@@ -55,105 +66,134 @@ public class Output {
 	
 	
 	 /**
-	 * Method to write densities into a PPM file. 
+	 * Method to write densities of animals into a PPM file.
+	 * The densities are rescaled so that the maximum value is 255
+	 * and used as intensity values (three for each pixel, for red, green and blue). 
 	 * @param outputName
 	 * @param density
 	 * @param colour
 	 **/
 	
-	public void printPpm(String outputName, double[][] density,
-			int[][] neighbours) {
-		/**
-		 * Calculate the maximum density.
-		 **/
+	public void printPpm(String outputName, double[][] density, int[][] neighbours) {
+		
+		
 		maxValue = 0;
 		minValue = 10; // have to guess at a good starting point here
-		for (x = 1; x < (density[0].length - 1); x++) {
-			for (y = 1; y < (density.length - 1); y++) {
-				if (density[y][x] > maxValue) {
+		
+		for (x = 1; x < (density[0].length - 1); x++) 
+		{
+			for (y = 1; y < (density.length - 1); y++) 
+			{
+				/**
+				 * Calculate maximum density.
+				 **/
+				if (density[y][x] > maxValue)
+				{
 					maxValue = density[y][x];
 				}
-				if ((density[y][x] < minValue) && (neighbours[y][x] != -1)) {
+				
+				/**
+				 * Calculate minimum density.
+				 **/
+				if ((density[y][x] < minValue) && (neighbours[y][x] != -1)) 
+				{
 					minValue = density[y][x];
 				}
 			}
 		}
-		double scale = 250 / (maxValue - minValue); // actual max will be 255,
+		
+		/**
+		 * Compute the scale factor.
+		 **/
+		double scale = 255 / (maxValue - minValue); 
 													// leave room for error
 
 		/**
-		 * Print the values in a ppm file.
+		 * Print the values in a PPM file.
 		 **/
-
 		BufferedWriter bufferedWriter = null;
+		
 		try {
-			bufferedWriter = new BufferedWriter(
-					new FileWriter(outputName, true));
-			bufferedWriter.write("P3");
+				bufferedWriter = new BufferedWriter(new FileWriter(outputName, true));
+			
+				bufferedWriter.write("P3");
+				bufferedWriter.newLine();
+			
+				bufferedWriter.write((density[0].length - 2) + " " + (density.length - 2));
+				bufferedWriter.newLine();
+			
+				bufferedWriter.write("255");
+				bufferedWriter.newLine();
 
-			bufferedWriter.newLine();
-			bufferedWriter.write((density[0].length - 2) + " "
-					+ (density.length - 2));
-			bufferedWriter.newLine();
-			bufferedWriter.write("255");
-			bufferedWriter.newLine();
-
-			for (y = 1; y < (density.length - 1); y++) {
-				for (x = 1; x < (density[0].length - 1); x++) {
-
-					/**
-					 * Fill water cells with blue.
-					 **/
-					if (neighbours[y][x] == -1) {
-						bufferedWriter.write("0 0 255 ");
-					} else {
-						bufferedWriter
-								.write((int) ((density[y][x] - minValue) * scale)
+				for (y = 1; y < (density.length - 1); y++) 
+				{
+					for (x = 1; x < (density[0].length - 1); x++)
+					{
+						
+						/**
+						 * Fill water cells with blue.
+						 **/
+						if (neighbours[y][x] == -1) 
+						{
+							bufferedWriter.write("0 0 255 ");
+						}
+						
+						/**
+						 * Fill land cells with grey with higher intensity corresponding to 
+						 * greater value of density in a given cell.
+						 **/
+						else
+						{
+							bufferedWriter.write((int) ((density[y][x] - minValue) * scale)
 										+ " "
 										+ (int) ((density[y][x] - minValue) * scale)
 										+ " "
 										+ (int) ((density[y][x] - minValue) * scale)
 										+ " ");
+						}
+
 					}
-
+					bufferedWriter.newLine();
 				}
-				bufferedWriter.newLine();
-			}
-			bufferedWriter.flush();
-			bufferedWriter.close();
-		} catch (IOException ioe) {
+				bufferedWriter.flush();
+				bufferedWriter.close();
+			} 
+		catch (IOException ioe) {
 			ioe.getMessage();
+			}
 		}
-	}
 
+	
 	/**
-	 * Method to write average density with corresponding time to a file
-	 * 
+	 * Method to write the average density with corresponding time to a file.
 	 * @param outputName
 	 * @param density
 	 * @param time
 	 **/
-	public void printMeanDensity(String outputName, double[][] density,
-			double time) {		
+	
+	public void printMeanDensity(String outputName, double[][] density,double time) {		
+		
 		double sum = 0;
 		double average;
+		
 		PrintWriter out = null;
 		
 		try {
-			out = new PrintWriter(new FileWriter(outputName,
-					true));
-		} catch (IOException ioe) {
+			out = new PrintWriter(new FileWriter(outputName,true));
+			} 
+		catch (IOException ioe) {
 			ioe.getMessage();
-		}
+			}
 
-		for (x = 1; x < (density[0].length - 1); x++) {
-			for (y = 1; y < (density.length - 1); y++) {
+		for (x = 1; x < (density[0].length - 1); x++) 
+		{
+			for (y = 1; y < (density.length - 1); y++) 
+			{
 				sum += density[y][x];
 			}
 		}
 
-		average = sum
-				/ ((double) (density.length - 1) * (double) (density[0].length - 1));
+		average = sum/ ((double) (density.length - 1) * (double) (density[0].length - 1));
 
 		out.printf("%f %f\n", time, average);
 		out.close();
